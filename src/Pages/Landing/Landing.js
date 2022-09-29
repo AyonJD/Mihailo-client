ee8d6619e17e2fca24ed616edcf40a97bf3f9467
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-29 23:35:52 +0600

    landing page done

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -5,24 +5,29 @@ import './Landing.css';
 const Landing = () => {
     return (
         <div className='mid-container'>
-            <div className='landing_header_section'>
-                <h1 className='landing_header'>
-                    Imagine if <br />
-                    <span className='snapchat'>Snapchat</span> <br />
-                    had events.
-                </h1>
-            </div>
-            <p className='landing_text'>
-                Easily host and share events with your friends across any social media.
-            </p>
+            <div className="landing_page">
+                <div className='content_section'>
+                    <div className='landing_header_section'>
+                        <h1 className='landing_header'>
+                            Imagine if <br />
+                            <span className='snapchat'>Snapchat</span> <br />
+                            had events.
+                        </h1>
+                    </div>
+                    <p className='landing_text'>
+                        Easily host and share events with your friends across any social media.
+                    </p>
+                    <div className='create_event'>
+                        <button className='create_event_button'>🎉 Create my event</button>
+                    </div>
+                </div>
 
-            <div className='landing_event_image'>
-                {/* <img src={popcornImage} alt="" /> */}
+                <div className='landing_event_image'>
+                    {/* <img src={popcornImage} alt="" /> */}
+                </div>
             </div>
 
-            <div className='create_event'>
-                <button className='create_event_button'>🎉 Create my event</button>
-            </div>
+
         </div>
     );
 };
