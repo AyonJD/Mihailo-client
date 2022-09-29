903d5ad35a527cde45e003a59a9597fb1de9e9c3
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-29 23:01:14 +0600

    tablet responsive done

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -5,11 +5,13 @@ import './Landing.css';
 const Landing = () => {
     return (
         <div className='mid-container'>
-            <h1 className='landing_header'>
-                Imagine if <br />
-                <span className='snapchat'>Snapchat</span> <br />
-                had events.
-            </h1>
+            <div className='landing_header_section'>
+                <h1 className='landing_header'>
+                    Imagine if <br />
+                    <span className='snapchat'>Snapchat</span> <br />
+                    had events.
+                </h1>
+            </div>
             <p className='landing_text'>
                 Easily host and share events with your friends across any social media.
             </p>
